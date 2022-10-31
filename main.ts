6675c1fa60f29b31a78f7fe5f304b393f1476654
kasahala <4k.kasahara@gmail.com>
2022-10-31 15:45:35 +0900

    feature: implements basic functions

--- main.ts
+++ main.ts
@@ -1,89 +1,53 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Plugin, PluginSettingTab, Setting } from "obsidian";
 
-// Remember to rename these classes and interfaces!
+import dirtreeist, { Options as DirtreeistOptions } from "@k4a_l/dirtreeist";
 
-interface MyPluginSettings {
-	mySetting: string;
+interface DirtreeistSettings {
+	treeType: Exclude<DirtreeistOptions["treeType"], undefined>;
+	emptyBeforeUpperHierarche: Exclude<
+		DirtreeistOptions["emptyBeforeUpperHierarche"],
+		undefined
+	>;
+	spaceBeforeName: Exclude<DirtreeistOptions["spaceBeforeName"], undefined>;
+	spaceSize: Exclude<DirtreeistOptions["spaceSize"], undefined>;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
+const DEFAULT_SETTINGS: DirtreeistSettings = {
+	treeType: "ascii",
+	emptyBeforeUpperHierarche: false,
+	spaceBeforeName: true,
+	spaceSize: 2,
+};
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class Dirtreeist extends Plugin {
+	settings: DirtreeistSettings;
 
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+		this.registerMarkdownCodeBlockProcessor(
+			"dirtree",
+			(source, el, ctx) => {
+				const result = dirtreeist(source, this.settings);
+				const div = el.createEl("div");
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
+				div.innerText = result.reduce((prev, dirtree, index) => {
+					return prev + (index !== 0 ? "\n\n" : "") + dirtree;
+				});
 			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
+		);
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		this.addSettingTab(new DirtreeistSettingTab(this.app, this));
 	}
 
-	onunload() {
-
-	}
+	onunload() {}
 
 	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+		this.settings = Object.assign(
+			{},
+			DEFAULT_SETTINGS,
+			await this.loadData()
+		);
 	}
 
 	async saveSettings() {
@@ -91,47 +55,70 @@ export default class MyPlugin extends Plugin {
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
+class DirtreeistSettingTab extends PluginSettingTab {
+	plugin: Dirtreeist;
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: Dirtreeist) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
 
 	display(): void {
-		const {containerEl} = this;
+		const { containerEl } = this;
 
 		containerEl.empty();
 
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
+		const treeTypeOptions: Record<string, DirtreeistSettings["treeType"]> =
+			{
+				normal: "normal",
+				bold: "bold",
+				ascii: "ascii",
+			};
+		new Setting(containerEl).setName("treeType").addDropdown((text) =>
+			text
+				.addOptions(treeTypeOptions)
+				.setValue(this.plugin.settings.treeType)
+				.onChange(async (value: DirtreeistSettings["treeType"]) => {
+					this.plugin.settings.treeType = value;
+					await this.plugin.saveSettings();
+				})
+		);
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
+			.setName("emptyBeforeUpperHierarche")
+			.addToggle((text) =>
+				text
+					.setValue(this.plugin.settings.emptyBeforeUpperHierarche)
+					.onChange(
+						async (
+							value: DirtreeistSettings["emptyBeforeUpperHierarche"]
+						) => {
+							this.plugin.settings.emptyBeforeUpperHierarche =
+								value;
+							await this.plugin.saveSettings();
+						}
+					)
+			);
+
+		new Setting(containerEl).setName("spaceBeforeName").addToggle((text) =>
+			text
+				.setValue(this.plugin.settings.spaceBeforeName)
+				.onChange(
+					async (value: DirtreeistSettings["spaceBeforeName"]) => {
+						this.plugin.settings.spaceBeforeName = value;
+						await this.plugin.saveSettings();
+					}
+				)
+		);
+
+		new Setting(containerEl).setName("spaceSize").addDropdown((text) =>
+			text
+				.addOptions({ "1": "1", "2": "2", "3": "3", "4": "4" })
+				.setValue(String(this.plugin.settings.spaceSize))
+				.onChange(async (value: String) => {
+					this.plugin.settings.spaceSize = Number(value);
 					await this.plugin.saveSettings();
-				}));
+				})
+		);
 	}
 }
